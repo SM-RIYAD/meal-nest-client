de907c1eb9fd1c62e639b907d2e996b32d1622e6
SM RIYAD <smriyad0185@gmail.com>
2024-01-14 19:10:25 +0600

    added admin credentials to the project

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -4,8 +4,9 @@ import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import "./Header.css";
 import useAdmin from "../hooks/useAdmin";
+import Swal from "sweetalert2";
 const Header = () => {
-   const [isAdmin] = useAdmin();
+  const [isAdmin] = useAdmin();
   const { user, logOut } = useContext(AuthContext);
   const { pathname } = useLocation();
   console.log("this is from header", pathname);
@@ -18,6 +19,43 @@ const Header = () => {
         navigate("/home");
       })
       .catch((err) => console.log(err));
+  };
+  const handleAdminCredentials = () => {
+    Swal.fire({
+      icon: "warning",
+      title: "Admin Credentials",
+      html: `
+      <div >
+    <p>Admin Email: syed@gmail.com </p> <p className='pt-5'>Admin Password: syeD123#</p>
+
+      </div>
+    `, showConfirmButton: false,
+      showCloseButton: true,
+      footer: "Log in as admin to explore admin dashboard",
+    });
+    // Swal.fire({
+    //   title: "<strong>Admin Credentials</strong>",
+    //   icon: "info",
+    //   html: `
+
+    //     <div >
+    //   <p>Admin Email: syed@gmail.com </p> <p>   Admin Password: syeD123#</p>
+    // <footer>Log in as admin to explore admin dashboard</footer>
+    //     </div>,
+
+    //   `,
+    //   showCloseButton: true,
+    //   showCancelButton: true,
+    //   focusConfirm: false,
+    //   confirmButtonText: `
+    //     <i class="fa fa-thumbs-up"></i> Great!
+    //   `,
+    //   confirmButtonAriaLabel: "Thumbs up, great!",
+    //   cancelButtonText: `
+    //     <i class="fa fa-thumbs-down"></i>
+    //   `,
+    //   cancelButtonAriaLabel: "Thumbs down"
+    // });
   };
 
   const navLinks = (
@@ -46,59 +84,53 @@ const Header = () => {
           Upcoming Meals
         </Link>
       </li>
-
-      
+      <li className={`${user ? "pt-5" : "pt-5"} `}>
+        <button className="admin-text" onClick={handleAdminCredentials}> Admin</button>
+      </li>
     </>
   );
 
   const endlinks = (
     <>
       <li>
-        {
-          user ? (
-          
-            <details>
-              <summary>
-
-                   <div
-                  className="w-12 rounded-full  tooltip"
-               
+        {user ? (
+          <details>
+            <summary>
+              <div className="w-12 rounded-full  tooltip">
+                <img className="rounded-full" src={user?.photoURL} />
+              </div>
+            </summary>
+            <ul className="p-2 bg-white ">
+              <li>
+                <a>{user?.displayName}</a>
+              </li>
+              <li className="hover:text-white">
+                <Link
+                  to={`${
+                    isAdmin === true ? "/dashboard" : "/dashboard/userProfile"
+                  }`}
                 >
-                  <img className="rounded-full" src={user?.photoURL} />
-                </div>
-
-                
-              </summary>
-              <ul className="p-2 bg-white ">
-                <li>
-                  <a>{user?.displayName}</a>
-                </li>
-                <li className="hover:text-white">
-                  <Link to={`${isAdmin===true? "/dashboard": "/dashboard/userProfile"}`}> 
                   <a>Dashboard</a>
-                  </Link>
-                
-                </li>
-                <li>
-                    <btn
+                </Link>
+              </li>
+              <li>
+                <btn
                   onClick={handleLogout}
                   className={`border-0  btn btn-error btn-outline`}
                 >
                   Logout
                 </btn>
-                </li>
-              </ul>
-            </details>
-          ) : (
-            <Link
-              className={`${pathname === "/login" ? "active-link" : ""}`}
-              to="/login"
-            >
-              <p className="border-0  font-bold">Log in</p>
-            </Link>
-          )
-   
-        }
+              </li>
+            </ul>
+          </details>
+        ) : (
+          <Link
+            className={`${pathname === "/login" ? "active-link" : ""}`}
+            to="/login"
+          >
+            <p className="border-0  font-bold">Log in</p>
+          </Link>
+        )}
       </li>
     </>
   );
@@ -131,28 +163,24 @@ const Header = () => {
               {endlinks}
             </ul>
           </div>
-        
-      <div className="lg:w-1/2 object-cover   w-full h-full">
 
-      <img className="lg:flex hidden object-cover" src="https://i.ibb.co/vZmgLmZ/mealnestlogo-removebg-preview-1.png" alt=""  />
+          <div className="lg:w-1/2 object-cover   w-full h-full">
+            <img
+              className="lg:flex hidden object-cover"
+              src="https://i.ibb.co/vZmgLmZ/mealnestlogo-removebg-preview-1.png"
+              alt=""
+            />
 
-
-      <p className="lg:hidden flex text-xl text-red-400 font-bold">
-
-MEAL NEST
-
-      </p>
-      </div>
-   
-        
+            <p className="lg:hidden flex text-xl text-red-400 font-bold">
+              MEAL NEST
+            </p>
+          </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-
           <ul className="menu menu-horizontal px-1">{navLinks}</ul>
         </div>
         <div className="navbar-end hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{endlinks}</ul>
-       
         </div>
       </div>
     </div>
